3f62bdc49aa1608701846af41403990b3a8fddc4
minzznguyen <94585907+minzznguyen@users.noreply.github.com>
2022-09-17 17:11:38 -0400

    Update data_scraping.py

--- data_scraping.py
+++ data_scraping.py
@@ -1,1 +1,8 @@
-print("hello world")
+import pandas as pd
+
+df = pd.read_excel('Stock.xlsx', sheetname=0) # can also index sheet by name or fetch all sheets
+price_list = df['Closed'].tolist()
+
+time_list = []
+for i in range(len(price_list)):
+    time_list.append(i+1)
