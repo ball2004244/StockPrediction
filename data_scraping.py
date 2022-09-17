fed2753482b61d1f2bdc936b01352e4b9a8517e4
minzznguyen <94585907+minzznguyen@users.noreply.github.com>
2022-09-17 17:35:31 -0400

    Finished the data_scrapping.py

--- data_scraping.py
+++ data_scraping.py
@@ -1,8 +1,24 @@
-import pandas as pd
-
-df = pd.read_excel('Stock.xlsx', sheetname=0) # can also index sheet by name or fetch all sheets
-price_list = df['Closed'].tolist()
+import csv
+ 
+# open the file in read mode
+filename = open('Stock.csv', 'r')
+ 
+# creating dictreader object
+file = csv.DictReader(filename)
+ 
+# creating empty lists
+price_list = []
+ 
+# iterating over each row and append
+# values to empty list
+for col in file:
+    price_list.append(col['Close'])
+    
 
 time_list = []
 for i in range(len(price_list)):
-    time_list.append(i+1)
+    time_list.append(i)
+
+print(time_list)
+print(price_list)
+
