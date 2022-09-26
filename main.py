78178733f1c4cdac3d8cf242697d3a8f84a05342
ball712004 <nguyenminhtam7124@gmail.com>
2022-09-26 17:40:30 -0400

    Update Third Hypothesis

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-'''COMPARE 1st and 2nd HYPO'''
+'''COMPARE HYPO'''
 from first_hypo import avg_predict
 from second_hypo import linear_predict
+from third_hypo import time_seg_predict
 from data_scraping import time_list, price_list
 import matplotlib.pyplot as plt
 
+
+test_points = [20, 100, 214, 265]
+# plt.plot(test_points, linear_predict(
+#     time_list, price_list, test_points), label='Pre Linear Predict')
+'''TEST IF THERE IS A DRAMATIC DROP IN PRICE'''
+drop = [60, 63, 62, 62.1, 61.5, 61.3]
+price_list += drop
+time_list += [251 + i for i in range(len(drop))]
+
 '''DRAW GRAPH'''
-test_points = [20, -57, 100, -9, 214]
 plt.plot(time_list, price_list, 'ro', label='Initial Function')
 plt.plot(test_points, avg_predict(time_list, price_list,
          test_points), label='Average Predict')
 plt.plot(test_points, linear_predict(
     time_list, price_list, test_points), label='Linear Predict')
+plt.plot(test_points, time_seg_predict(
+    time_list, price_list, test_points), label='Time Segmented Predict')
 plt.title('Stock Graph')
 plt.legend()
 plt.xlabel('x - axis')
